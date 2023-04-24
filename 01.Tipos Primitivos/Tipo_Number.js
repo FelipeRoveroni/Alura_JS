d72ca558d36b5851ba71fd6004b5b58408da2ae5
FelipeRoveroni <rm95430@fiap.com.br>
2023-04-24 18:24:04 -0300

    Primeiro Commit

---
+++ 01.Tipos Primitivos/Tipo_Number.js
@@ -0,0 +1,10 @@
+// Tipo Number
+
+const meuNumerio = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero * segundoNumero;
+
+console.log(operacaoMatematica)
