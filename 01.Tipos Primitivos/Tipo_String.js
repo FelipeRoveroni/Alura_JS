926170da22446a093e561fc68fbd1a01e81d7e7b
FelipeRoveroni <rm95430@fiap.com.br>
2023-04-24 18:37:13 -0300

    Tipo String Finalizado!

---
+++ 01.Tipos Primitivos/Tipo_String.js
@@ -0,0 +1,15 @@
+const texto1 = "Olá Mundo!";
+const texto2 = 'Olá Mundo!';
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "34567";
+
+const citacao = "Meu nome é ";
+const meuNome = "Felipe";
+console.log(citacao)
+
+// concatenação (+)
+
+console.log(citacao + meuNome)
+
+
+//template string OU template literal
