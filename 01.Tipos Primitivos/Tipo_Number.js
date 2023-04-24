f0bb992e084956b5ef0450d110d6fcac08280ce7
FelipeRoveroni <rm95430@fiap.com.br>
2023-04-24 18:28:03 -0300

    Tipo Number Finalizado!

--- 01.Tipos Primitivos/Tipo_Number.js
+++ 01.Tipos Primitivos/Tipo_Number.js
@@ -8,3 +8,17 @@ const segundoNumero = 2;
 const operacaoMatematica = primeiroNumero * segundoNumero;
 
 console.log(operacaoMatematica)
+
+// Ponto Flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(novaOperacao)
+
+// NaN -> not a number (Não é um numero)
+
+const alura = "Alura";
+console.log(alura * primeiroNumero) 
